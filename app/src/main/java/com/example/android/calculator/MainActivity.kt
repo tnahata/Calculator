3633fb838038078b78cb191e22b91f39b2a63408
tnahata <tanishnahata2002@gmail.com>
2022-01-11 18:16:16 +0530

    added a comment to see commits working

--- app/src/main/java/com/example/android/calculator/MainActivity.kt
+++ app/src/main/java/com/example/android/calculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // sets the content of the layout for the main activity
         setContentView(R.layout.activity_main)
     }
 }
